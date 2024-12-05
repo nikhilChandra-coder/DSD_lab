de06caf98d757c217c8cbf93448195a781b77cbb
nikhilChandra-coder <67769125+nikhilChandra-coder@users.noreply.github.com>
2024-12-05 15:13:54 -0500

    Add files via upload

---
+++ lab6-Single Path RISC Architecture/lab6-final/ALU.sv
@@ -0,0 +1,14 @@
+module ALU(
+	input logic[31:0] SrcA, SrcB,
+	input logic[2:0] ALUControl,
+	output logic[31:0] ALUResult
+);
+
+	always_comb begin
+        case (ALUControl)
+            3'b010: ALUResult = SrcA + SrcB;
+            3'b110: ALUResult = SrcA - SrcB;
+            default: ALUResult = 32'b0;  
+        endcase
+    end
+endmodule
